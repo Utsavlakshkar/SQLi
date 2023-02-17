bc5ec8c6c8c26c12c8c35fd2e86001cd46f908d8
Utsavlakshkar <82568016+Utsavlakshkar@users.noreply.github.com>
2023-02-17 23:56:41 +0530

    DB Updated

--- Login-db.php
+++ Login-db.php
@@ -1,7 +1,13 @@
 <?php 
-    $name = $_GET["uname"];                
-    $age = $_GET["password"];                                
-    $con=mysqli_connect("localhost","root","","utsav");
-    $sql="INSERT INTO student(Name,Age,Branch_ID) VALUES('".$name."','".$age."','".$branch."');";
+    $uname = $_GET["uname"];                
+    $password = $_GET["password"];                                
+    $con=mysqli_connect("localhost","root","","test");
+    $sql="SELECT order_id,item_name FROM `purchase` WHERE user_id=(SELECT user_id FROM users WHERE user_name='".$uname."')and user_id=(SELECT user_id FROM passkeys WHERE passk='".$password."');";
     $result=$con->query($sql);
+    if($result->num_rows>0){
+        echo "Login Successful";
+    }
+    else{
+        echo "Invalid Login Username or Password";
+    }
 ?>
